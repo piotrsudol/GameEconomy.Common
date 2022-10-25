d6476e6fa6154223298c17aa1890b31d07d2e377
piotr.olender <piotr.olender@shijigroup.com>
2022-10-25 20:07:04 +0200

    Core

---
+++ GameEconomy.Common/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace GameEconomy.Common
+{
+    public static class Configuration
+    {
+        public static T SettingsFromConfiguration<T>(this IConfiguration configuration) where T : new()
+        {
+            return configuration.GetSection(typeof(T).Name).Get<T>();
+        }
+    }
+}
---
+++ GameEconomy.Common/MongoDB/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using GameEconomy.Common.Settings;
+
+namespace GameEconomy.Common.MongoDB
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMongo(this IServiceCollection services)
+        {
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+            services.AddSingleton(serviceProvider =>
+            {
+                var configuration = serviceProvider.GetService<IConfiguration>();
+                var serviceSettings = configuration.SettingsFromConfiguration<ServiceSettings>();
+                var mongoDbSettings = configuration.SettingsFromConfiguration<MongoDbSettings>();
+                var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
+                return mongoClient.GetDatabase(serviceSettings.ServiceName);
+            });
+
+            return services;
+
+        }
+
+        public static IServiceCollection AddMongoRepository<T>(this IServiceCollection services, string collectionName) where T : IEntity
+        {
+
+            services.AddSingleton<IRepository<T>>(serviceProvider =>
+            {
+                var database = serviceProvider.GetService<IMongoDatabase>();
+                return new MongoRepository<T>(database, collectionName);
+            });
+
+            return services;
+        }
+    }
+}
